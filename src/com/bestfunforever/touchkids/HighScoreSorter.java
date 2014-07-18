b193d91d7e34ab9126c4cd35956c6d4e94abece0
unknown <tgioihan@gmail.com>
2014-07-18 11:52:14 +0700

    cm

--- src/com/bestfunforever/touchkids/HighScoreSorter.java
+++ src/com/bestfunforever/touchkids/HighScoreSorter.java
@@ -8,12 +8,12 @@ public class HighScoreSorter implements Comparator<HighScore> {
 	public int compare(HighScore lhs, HighScore rhs) {
 		if (lhs.getScore() > rhs.getScore()) {
 			return -1;
-		} else if (lhs.getScore() > rhs.getScore()) {
+		} else if (lhs.getScore() < rhs.getScore()) {
 			return +1;
 		} else {
 			if (lhs.getCreatedTime() > rhs.getCreatedTime()) {
 				return -1;
-			} else if (lhs.getCreatedTime() > rhs.getCreatedTime()) {
+			} else if (lhs.getCreatedTime() < rhs.getCreatedTime()) {
 				return 1;
 			} else {
 				return 0;
